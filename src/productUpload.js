25a0f13f69c6f894a2a1a9a5c28674cc00d16b11
Reid South <rbsouth@gmail.com>
2018-06-12 20:46:18 -0600

    form displayed on page

--- src/productUpload.js
+++ src/productUpload.js
@@ -1,18 +1,31 @@
+import React, { Component } from 'react';
+
 class ProductUpload extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      value: '',
+      takeAll: false
+    };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+
   }
 
   handleChange(event) {
     this.setState({value: event.target.value});
   }
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+  }
+
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('A product was submitted: ' + this.state.value);
     event.preventDefault();
   }
 
@@ -21,10 +34,24 @@ class ProductUpload extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input 
+          type="text" 
+          value={this.state.value} 
+          onChange={this.handleChange} />
+        </label>
+        <br />
+        <label>
+          Take All:
+          <input
+            name="takeAll"
+            type="checkbox"
+            checked={this.state.takeAll}
+            onChange={this.handleInputChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
     );
   }
 }
+
+export default ProductUpload;
