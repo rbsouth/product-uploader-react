43051d49bfd6f2038e6dab32460a502f21132636
Reid South <rbsouth@gmail.com>
2018-06-26 20:07:37 -0600

    product upload form sends information through

--- src/productUpload.js
+++ src/productUpload.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './ProductUpload.css';
 
 class ProductUpload extends React.Component {
   constructor(props) {
@@ -14,42 +15,101 @@ class ProductUpload extends React.Component {
 
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleChange(e) {
+    this.setState({value: e.target.value});
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+  handleInputChange(e) {
+    const target = e.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
   }
 
-  handleSubmit(event) {
+  handleSubmit(e) {
     alert('A product was submitted: ' + this.state.value);
-    event.preventDefault();
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+    console.log('handle uploading-', this.state.file);
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file)
   }
 
   render() {
+    let {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = (<img src={imagePreviewUrl} />);
+    } else {
+      $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
+    }
+
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input 
-          type="text" 
-          value={this.state.value} 
-          onChange={this.handleChange} />
-        </label>
-        <br />
-        <label>
-          Take All:
-          <input
-            name="takeAll"
-            type="checkbox"
-            checked={this.state.takeAll}
-            onChange={this.handleInputChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="product-uploader">
+        <div className="previewComponent">
+          <form onSubmit={(e)=>this.handleSubmit(e)}>
+            <div className="imgPreview">
+            {$imagePreview}
+            </div>
+            <input className="fileInput" 
+              type="file" 
+              onChange={(e)=>this._handleImageChange(e)} />
+              <br />
+            <label>
+              Title:
+              <input 
+              type="text"
+              value={this.state.value} 
+              onChange={this.handleChange} />
+            </label>
+            <br />
+            <label>
+              Price Per Unit:
+              <input 
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange} />
+            </label>
+            <label>
+              Number of Units:
+              <input 
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange} />
+            </label>
+            <br />
+            <label>
+              Description:
+              <textarea value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <br />
+            <label>
+              Take All:
+              <input
+                name="takeAll"
+                type="checkbox"
+                checked={this.state.takeAll}
+                onChange={this.handleInputChange} />
+            </label>
+            <button className="submitButton" 
+              type="submit" 
+              onClick={(e)=>this.handleSubmit(e)}>Upload Product</button>
+          </form>
+        </div>
+      </div>
     );
   }
 }
